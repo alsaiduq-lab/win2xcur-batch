67c95b496275e439c39f4bfe6a6a7f4b545eb0d9
alsaiduq-lab <riiidge.racer@gmail.com>
2025-03-31 20:29:28 -0600

    refactored due to missing libs

--- converter.go
+++ converter.go
@@ -3,143 +3,88 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
-	"regexp"
+	"path/filepath"
+	"strings"
 )
 
-// Mappings are the top-level object holding
 type Mappings struct {
 	Pairs []Pair `json:"mappings"`
 }
 
-// Pair holds the corresponding Linux cursors for the Windows cursor
 type Pair struct {
 	Windows string   `json:"windows"`
 	Linux   []string `json:"linux"`
 }
 
 func main() {
+	fmt.Println("Starting converter...")
 	pwd, _ := os.Getwd()
-	windowsDir := "Unzipped" // Replace with the path to your directory
-	fullPathToWindowsDir := pwd + "/" + windowsDir
+	windowsDir := "Unzipped"
+	fullPathToWindowsDir := filepath.Join(pwd, windowsDir)
+	fmt.Println("Looking in:", fullPathToWindowsDir)
 
-	// Read windows to linux mappings from file
-	file, _ := ioutil.ReadFile("map.json")
-	mapped := Mappings{}
-	_ = json.Unmarshal([]byte(file), &mapped)
-
-	// Read directory containing original files
-	directories, lsErr := os.ReadDir(fullPathToWindowsDir)
-	if lsErr != nil {
-		fmt.Println(lsErr)
+	file, err := os.ReadFile("map.json")
+	if err != nil {
+		fmt.Println("Error reading map.json:", err)
 		return
 	}
+	var mapped Mappings
+	if err := json.Unmarshal(file, &mapped); err != nil {
+		fmt.Println("Error unmarshaling map.json:", err)
+		return
+	}
+	fmt.Println("Loaded map.json with", len(mapped.Pairs), "mappings")
 
-	// Convert from Windows to Linux
 	convertDir := "Converted"
-	fullPathToConvertedDir := pwd + "/" + convertDir
-	convertMkdirErr := os.MkdirAll(fullPathToConvertedDir, 0700)
-	if convertMkdirErr != nil {
-		fmt.Println(convertMkdirErr)
+	fullPathToConvertedDir := filepath.Join(pwd, convertDir)
+	if err := os.MkdirAll(fullPathToConvertedDir, 0700); err != nil {
+		fmt.Println("Error creating Converted dir:", err)
 		return
 	}
-	converterFolderRequirement := regexp.MustCompile(`[^a-zA-Z0-9]+`)
-	for i := range directories {
-		if !directories[i].IsDir() {
+
+	files, err := os.ReadDir(fullPathToWindowsDir)
+	if err != nil {
+		fmt.Println("Error reading Unzipped dir:", err)
+		return
+	}
+	fmt.Println("Found", len(files), "items in Unzipped")
+
+	for _, file := range files {
+		fileName := file.Name()
+		if file.IsDir() {
+			fmt.Println("Skipping directory:", fileName)
 			continue
 		}
-		dirName := directories[i].Name()
-		fullPathToDir := fullPathToWindowsDir + "/" + dirName
-		relativePathToDir := windowsDir + "/" + dirName
-		matched := converterFolderRequirement.Match([]byte(dirName))
-		if matched {
-			dirName = converterFolderRequirement.ReplaceAllString(dirName, "")
-			relativePathToDir = windowsDir + "/" + dirName
-			renameErr := os.Rename(fullPathToDir, pwd+"/"+relativePathToDir)
-			if renameErr != nil {
-				fmt.Println(renameErr)
-				return
-			}
+		if strings.HasPrefix(fileName, ".") {
+			fmt.Println("Skipping hidden file:", fileName)
+			continue
 		}
+		if filepath.Ext(fileName) != ".ani" {
+			fmt.Println("Skipping non-.ani file:", fileName)
+			continue
+		}
+		inputPath := filepath.Join(fullPathToWindowsDir, fileName)
+		baseName := strings.TrimSuffix(fileName, ".ani")
+		outputPath := filepath.Join(fullPathToConvertedDir, baseName+".xcur")
+		tempOutput := filepath.Join(fullPathToConvertedDir, baseName)
 
-		convertMkdirErr := os.MkdirAll(fullPathToConvertedDir+"/"+dirName, 0700)
-		if convertMkdirErr != nil {
-			fmt.Println(convertMkdirErr)
-			return
-		}
-		convertCmd := fmt.Sprintf("win2xcur %s/*.ani -o %s", relativePathToDir, convertDir+"/"+dirName)
-		fmt.Println(convertCmd)
-		cmd := exec.Command("bash", "-c", convertCmd)
-		err := cmd.Run()
+		fmt.Println("Converting:", inputPath, "to:", outputPath)
+		cmd := exec.Command("win2xcur", inputPath, "-o", fullPathToConvertedDir)
+		output, err := cmd.CombinedOutput()
 		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
-
-	// Sort converted files
-	sortDir := pwd + "/" + "Sorted"
-	sortedMkdirErr := os.MkdirAll(sortDir, 0700)
-	if sortedMkdirErr != nil {
-		fmt.Println(sortedMkdirErr)
-		return
-	}
-	directories, lsErr = os.ReadDir(fullPathToConvertedDir)
-	if lsErr != nil {
-		fmt.Println(lsErr)
-		return
-	}
-	for i := range directories {
-		fmt.Println("Sorting " + directories[i].Name())
-		linuxDir := sortDir + "/" + directories[i].Name()
-		orgDir := fullPathToConvertedDir + "/" + directories[i].Name()
-		linuxCursorDir := linuxDir + "/" + "cursors"
-		os.Mkdir(linuxDir, 0744)
-		os.Mkdir(linuxCursorDir, 0744)
-		winFiles, lsErr := os.ReadDir(orgDir)
-		if lsErr != nil {
-			fmt.Println(lsErr)
-			return
+			fmt.Println("Error converting", fileName, ":", err)
+			fmt.Println("win2xcur output:", string(output))
+			continue
 		}
 
-		for _, winFile := range winFiles {
-			for _, pair := range mapped.Pairs {
-				if pair.Windows == winFile.Name() {
-					for index, linuxName := range pair.Linux {
-						if index == 0 {
-							fmt.Println("  " + winFile.Name() + " -> " + linuxName)
-							input, err := ioutil.ReadFile(orgDir + "/" + winFile.Name())
-							if err != nil {
-								fmt.Println(err)
-								return
-							}
-
-							err = ioutil.WriteFile(linuxCursorDir+"/"+linuxName, input, 0644)
-							if err != nil {
-								fmt.Println("Error creating", linuxCursorDir+"/"+linuxName)
-								fmt.Println(err)
-								return
-							}
-						} else {
-							firstLinuxName := pair.Linux[0]
-							fmt.Println("  " + winFile.Name() + " -> " + linuxName + " (symlink to: " + firstLinuxName + ")")
-							os.Symlink(firstLinuxName, linuxCursorDir+"/"+linuxName)
-						}
-					}
-				}
-			}
+		if err := os.Rename(tempOutput, outputPath); err != nil {
+			fmt.Println("Error renaming", tempOutput, "to", outputPath, ":", err)
+			continue
 		}
-
-		// Add cursor.theme
-		cursorConfig := fmt.Sprintf("[Cursor Theme]\nName=%s\nInherits=Bibata-Modern-Classic\n", directories[i].Name())
-		byteCursorConfig := []byte(cursorConfig)
-		err := ioutil.WriteFile(linuxDir+"/"+"cursor.theme", byteCursorConfig, 0644)
-		if err != nil {
-			fmt.Println("Error creating", linuxDir+"/"+"cusror.theme")
-			fmt.Println(err)
-			return
-		}
+		fmt.Println("Successfully converted", fileName, "to", outputPath)
 	}
+
+	fmt.Println("Done!")
 }
